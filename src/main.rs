080c19a20568dbce206c04fd8bd7932905906387
Luky Drum <lukbubi12@gmail.com>
2024-11-21 15:15:55 +0100

    Setup axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // Build the app using 'route'
+    let app = Router::new().route("/", get(root));
+
+    let uri = "127.0.0.1:8080";
+    // Run app
+    let listener = tokio::net::TcpListener::bind(uri).await.unwrap();
+
+    println!("\nServer running on {}...", uri);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("<h1>Video-optimized web server</h1>")
 }
