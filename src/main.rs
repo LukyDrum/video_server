af46e44fb26a4e09b03f19b9817e1f9a9b4aaf0b
Luky Drum <lukbubi12@gmail.com>
2024-11-22 14:27:12 +0100

    Read data from put and put them inside a single storage object

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,77 @@
-use axum::{response::Html, routing::get, Router};
+mod storage_object;
+
+use axum::{
+    extract::{Request, State},
+    response::Html,
+    routing::{get, put},
+    Router,
+};
+use futures::StreamExt;
+
+use storage_object::StorageObject;
+
+
+#[derive(Clone)]
+struct ServerState {
+    pub obj: StorageObject
+}
+
+impl ServerState {
+    pub fn new() -> Self {
+        ServerState { obj: StorageObject::new("every".to_string()) }
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    // Init state
+    let state = ServerState::new();
+
     // Build the app using 'route'
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/*filename", put(upload))
+        .with_state(state);
 
-    let uri = "127.0.0.1:8080";
+    let url = "127.0.0.1:8080";
     // Run app
-    let listener = tokio::net::TcpListener::bind(uri).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(url).await.unwrap();
 
-    println!("\nServer running on {}...", uri);
+    println!("\nServer running on {}...", url);
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> Html<&'static str> {
     Html("<h1>Video-optimized web server</h1>")
+}
+
+async fn upload(State(state): State<ServerState>, request: Request) -> () {
+    // Parse the URI into path and file(name)
+    let mut uri_parts: Vec<&str> = request.uri().path().split_terminator('/').collect();
+    let file = uri_parts.pop().unwrap().to_string();
+    let path = uri_parts.join("/");
+    
+    // Convert body to stream
+    let mut stream = request.into_body().into_data_stream();
+
+    // Get new (last) segment in storage object
+    let segment = state.obj.new_segment();
+    // Loop trough stream, wait for bytes and add the bytes to last segment
+    loop {
+        if let Some(Ok(bytes)) = stream.next().await {
+            // Update metadata (eg. .mpd)
+            if file.ends_with(".mpd") {
+                state.obj.update_meta(bytes);
+            }
+            // Handle chunks
+            else {
+                // Get write lock for segment
+                let mut write = segment.write().unwrap();
+                write.push(bytes);
+            }
+
+        } else {
+            break;
+        }
+    }
 }
